ac73412867233e3fedb449f4c295db5c537a2058
OCRV\konstantin.zhukov <konstantin.zhukov@ocrv.ru>
2021-04-19 16:01:53 +0300

    Добавил генирацию скорости случайным образом.

--- Assets/Scrips/Rotate.cs
+++ Assets/Scrips/Rotate.cs
@@ -6,9 +6,12 @@ public class Rotate : MonoBehaviour
 {
     public float speed = 10f;
 
+    void Start(){
+        speed = Random.Range(0, 1);
+    }
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.09f);
+        transform.Rotate(0, 0, speed);
     }
 }
